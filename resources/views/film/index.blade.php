682416d0f588bc0548f9845be1114d41fcde06ec
Farhan Sangaji <farhan11soyae@gmail.com>
2025-01-04 18:28:59 +0700

    feat: update film index view to display dynamic film data with poster and synopsis

--- resources/views/film/index.blade.php
+++ resources/views/film/index.blade.php
@@ -2,20 +2,18 @@
 @section('content')
 
 <div class="grid grid-cols-1 lg:grid-cols-3 gap-3">
+    @foreach ($film as $f)
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
         <a href="#">
-            <img class="rounded-t-lg" src="https://cdn.pixabay.com/photo/2020/02/15/04/19/chicken-4849979_1280.jpg"
-                alt="" />
+            <img class="rounded-t-lg" src="{{ asset('storage/' . $f->poster) }}"
+                alt="poster untuk judul film {{ $f->judul }}" />
         </a>
         <div class="p-5">
             <a href="#">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">Noteworthy technology
-                    acquisitions 2021</h5>
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900">{{ $f->judul }}</h5>
             </a>
-            <p class="mb-3 text-sm font-normal text-gray-700">Here are the biggest enterprise
-                technology
-                acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="#"
+            <p class="mb-3 text-sm font-normal text-gray-700">{{ Str::limit($f->sinopsis, 100) }}</p>
+            <a href="{{ route('film.show', $f->id) }}"
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                 Lengkapnya
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -26,6 +24,7 @@
             </a>
         </div>
     </div>
+    @endforeach
 </div>
 
 @endsection
