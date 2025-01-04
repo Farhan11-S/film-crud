0801b4db93399fa224819a0d8f3300f59071b9e0
Farhan Sangaji <farhan11soyae@gmail.com>
2025-01-04 17:25:55 +0700

    feat: implement authentication with login and registration controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisterController::class, 'store']);
+
+    Route::get('login', [LoginController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [LoginController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [LoginController::class, 'destroy'])
+        ->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('film', FilmController::class);
+    Route::get('/', function () {
+        return redirect()->route('film.index');
+    });
 });
